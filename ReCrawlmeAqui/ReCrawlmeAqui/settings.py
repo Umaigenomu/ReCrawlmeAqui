0bce49ddacf12fa7dbf1ada435b9a19899d40b75
Ferch42 <fernando.hsieh@usp.br>
2018-09-18 11:53:02 -0300

    misaka chan kawaiiii222 primeiro teste

--- ReCrawlmeAqui/ReCrawlmeAqui/settings.py
+++ ReCrawlmeAqui/ReCrawlmeAqui/settings.py
@@ -9,14 +9,15 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'ReCrawlmeAqui'
+BOT_NAME = 'MisakaChan'
 
 SPIDER_MODULES = ['ReCrawlmeAqui.spiders']
 NEWSPIDER_MODULE = 'ReCrawlmeAqui.spiders'
 
+DEPTH_LIMIT = 7
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+# USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,13 +28,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
-CONCURRENT_REQUESTS_PER_IP = 8
+# CONCURRENT_REQUESTS_PER_IP = 8
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -70,16 +71,16 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = True
+# AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- ReCrawlmeAqui/ReCrawlmeAqui/spiders/MisakaChan.py
+++ ReCrawlmeAqui/ReCrawlmeAqui/spiders/MisakaChan.py
@@ -1,9 +1,7 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 import scrapy
-
-class ReCrawler(CrawlSpider):
-    name = "ReCrawler"
+import re
+class MisakaChan(scrapy.Spider):
+    name = "MisakaChan"
     allowed_domains = ['cidadao.reclameaqui.com.br', 'reclameaqui.com.br']
     # custom_settings = {
     #     'USER_AGENT': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
@@ -16,28 +14,44 @@ class ReCrawler(CrawlSpider):
 
     start_urls = (
             "https://cidadao.reclameaqui.com.br/",
-            "https://www.reclameaqui.com.br/"
+            # "https://www.reclameaqui.com.br/"
     )
-    rules = (
-        # Cidadao
-        Rule(LinkExtractor(allow=(r"cidadao\.reclameaqui.*",)), follow=True),
-        Rule(LinkExtractor(allow=(r"\d+/.*",)), callback='parse_cidadao', follow=True),
-        # Regular
-        # Rule(LinkExtractor(allow=r"www\.reclameaqui"), callback=lambda r: print(r.url), follow=True),
-        # Rule(LinkExtractor(allow="cidadao\.reclameaqui\.com\.br/\d+/.*"), callback='parseRegular', follow=True),
-    )
+
+    def start_requests(self):
+        for url in self.start_urls:
+            if url == "https://cidadao.reclameaqui.com.br/":
+                yield scrapy.Request(url, callback=self.parse_cidadao)
+
+    def parse(self, response):
+        # if response.url == "https://cidadao.reclameaqui.com.br/":
+        self.parse_cidadao(response)
+        # else:
+        #     self.parseRegular(response)
 
 
     def parse_cidadao(self, response):
         # Yield ONLY IF the page is eligible
         # if ...
-        return scrapy.Item({
+        for link in response.xpath('//a/@href').extract():
+            if re.search("\d+/.*", link):
+                yield response.follow(link, callback=self.return_item_cidadao)
+            if re.search("cidadao\.reclameaqui.*", link):
+                yield response.follow(link, callback=self.parse_cidadao)
+
+    def return_item_cidadao(self, response):
+
+        yield {
             "titulo": response.xpath("//p[@class='titulo']/text()").extract()[0],
             "reclamacao": "".join(response.xpath("//section[@class='paperblock']/p/text()").extract()),
             "local": response.xpath("//ul[@id='details']/li/p/a/text()").extract()[0],
             "categoria": response.xpath("//ul[@id='details']/li/p/a/text()").extract()[1],
             "time": response.xpath("//header[@class='header-verrec']/small/text()").extract()[0]
-        })
+        }
+        for link in response.xpath('//a/@href').extract():
+            if re.search("\d+/.*", link):
+                yield response.follow(link, callback=self.return_item_cidadao)
+            if re.search("cidadao\.reclameaqui.*", link):
+                yield response.follow(link, callback=self.parse_cidadao)
 
     def parseRegular(self, response):
         pass
